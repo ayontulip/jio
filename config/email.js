ae44dc41db2812c6ecfb373ac0048c00275b3cef
Ayon Saha <ayon@tulipinfotek.com>
2016-08-31 10:27:28 +0530

    Update email.js

--- config/email.js
+++ config/email.js
@@ -2,8 +2,8 @@
 module.exports.email = {
     host: 'smtp.gmail.com',
     port: 587,
-    user: 'skoolstarsupp@gmail.com',
-    password: 'skadmin@123',
+    user: 'demo@gmail.com',
+    password: 'password',
     templateDirectory: 'views/emailTemplates',
     from: 'Ayon Saha <jio@thecatalystindia.in>'
 };
